15e7b732befb2cd06614e25ac2b3a5a841dcb3ab
ABeltramo <ABeltramo@users.noreply.github.com>
2023-04-04 20:01:23 +0000

    Committing clang-format changes

--- src/input/input/input.hpp
+++ src/input/input/input.hpp
@@ -21,8 +21,7 @@ struct InputReady {
  * PLATFORM DEPENDENT
  * will wait for events on the event bus and setup virtual devices accordingly.
  */
-InputReady setup_handlers(std::size_t session_id,
-                          const std::shared_ptr<dp::event_bus> &event_bus);
+InputReady setup_handlers(std::size_t session_id, const std::shared_ptr<dp::event_bus> &event_bus);
 
 /**
  * A packet of type INPUT_DATA will have different shapes based on the type
--- src/input/platforms/unknown/input.cpp
+++ src/input/platforms/unknown/input.cpp
@@ -3,8 +3,7 @@
 
 namespace input {
 
-InputReady setup_handlers(std::size_t session_id,
-                          const std::shared_ptr<dp::event_bus> &event_bus) {
+InputReady setup_handlers(std::size_t session_id, const std::shared_ptr<dp::event_bus> &event_bus) {
   logs::log(logs::error, "Unable to setup input handlers for the current platform.");
   return {.devices_paths = {}, .registered_handlers = {}};
 }
