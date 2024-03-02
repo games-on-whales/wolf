36cb5caeb55ee00c06630587bc0f62d9b348d06a
Pierre beucher <pierre@crafteo.io>
2024-03-02 17:18:09 +0100

    fix: create cfg directory on container startup

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# Make sure configure folder exists
+# as Wolf may try to create default config in non existing folder and crash.
+# See https://github.com/games-on-whales/wolf/pull/65#discussion_r1509235307
+# and https://github.com/games-on-whales/wolf/issues/64#issuecomment-1951479056
+mkdir -p $WOLF_CFG_FOLDER
+
 # Update fake-udev if missing from the path
 export WOLF_DOCKER_FAKE_UDEV_PATH=${WOLF_DOCKER_FAKE_UDEV_PATH:-$HOST_APPS_STATE_FOLDER/fake-udev}
 cp /wolf/fake-udev $WOLF_DOCKER_FAKE_UDEV_PATH
