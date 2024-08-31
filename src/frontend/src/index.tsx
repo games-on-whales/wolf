264237485b05a1d1ff0fe985916efc67c52946da
Lasath Fernando <devel@lasath.org>
2024-08-31 13:42:21 -0500

    Experiement with visuals

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -4,16 +4,29 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ConfigProvider, theme } from "antd";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
+const { darkAlgorithm } = theme;
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ConfigProvider
+        theme={{
+          algorithm: darkAlgorithm,
+          token: {
+            colorPrimary: "#134990",
+            borderRadius: 0,
+          },
+        }}
+      >
+        <App />
+      </ConfigProvider>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
