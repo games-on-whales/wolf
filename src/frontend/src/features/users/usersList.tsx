61ea8f03e27404c0b298370abb645b88e2c2991d
Lasath Fernando <devel@lasath.org>
2024-09-04 19:16:06 -0500

    Refactor userlist

--- src/frontend/src/features/users/usersList.tsx
+++ src/frontend/src/features/users/usersList.tsx
@@ -1,12 +1,10 @@
 import { mdiAccountOutline, mdiAccountPlus } from "@mdi/js";
 import Icon from "@mdi/react";
 import { AspectRatio, Card, CardContent, Link } from "@mui/joy";
-import { FC, ReactNode } from "react";
+import type { FC } from "react";
 import { useGetApiUsersQuery } from "../backend/wolfBackend.generated";
 import { OVERLAY_SX } from "../sections/overlay";
 import { Section } from "../sections/section";
-
-const IMAGE_SIZE = 128;
 
 export const UsersList: FC = () => {
   const { data: usersList } = useGetApiUsersQuery();
@@ -17,56 +15,55 @@ export const UsersList: FC = () => {
         {usersList?.length === 0
           ? "There are no users on this server"
           : usersList?.map((user, index) => (
-              <Card
-                orientation="vertical"
-                variant="outlined"
+              <UserView
                 key={index}
-                sx={{ alignItems: "center" }}
-              >
-                <UserAspectRatio>
-                  {user.profileImage != null ? (
-                    <img src={user.profileImage} alt={user.name + "Avatar"} />
-                  ) : (
-                    <Icon path={mdiAccountOutline} size="64px" />
-                  )}
-                </UserAspectRatio>
-                <Link href={"#"} overlay sx={OVERLAY_SX}>
-                  {user.name}
-                </Link>
-              </Card>
+                profileImage={user.profileImage}
+                fallbackIcon={mdiAccountOutline}
+                name={user.name ?? "Unknown"}
+              />
             ))}
-        <Card
-          orientation="vertical"
-          variant="outlined"
-          sx={{ alignItems: "center" }}
-        >
-          <UserAspectRatio>
-            <UserIcon path={mdiAccountPlus} />
-          </UserAspectRatio>
-          <Link href={"#"} overlay sx={OVERLAY_SX}>
-            Add User
-          </Link>
-        </Card>
+        <UserView
+          profileImage={null}
+          fallbackIcon={mdiAccountPlus}
+          name="Add User"
+        />
       </CardContent>
     </Section>
   );
 };
 
-const UserAspectRatio: FC<{ children: ReactNode }> = ({ children }) => {
+interface IUserViewProps {
+  profileImage?: string | null;
+  fallbackIcon: string;
+  name: string;
+}
+const UserView: FC<IUserViewProps> = ({ profileImage, fallbackIcon, name }) => {
+  const imageSize = 128;
+  const iconWidth = imageSize * 0.5;
   return (
-    <AspectRatio ratio={1} sx={{ width: `${IMAGE_SIZE}px` }}>
-      {children}
-    </AspectRatio>
-  );
-};
-
-const UserIcon: FC<{ path: string }> = ({ path }) => {
-  const iconWidth = IMAGE_SIZE * 0.5;
-  return (
-    <Icon
-      path={path}
-      size={iconWidth + "px"}
-      style={{ left: iconWidth / 2, top: iconWidth / 2, position: "absolute" }}
-    />
+    <Card
+      orientation="vertical"
+      variant="outlined"
+      sx={{ alignItems: "center" }}
+    >
+      <AspectRatio ratio={1} sx={{ width: `${imageSize}px` }}>
+        {profileImage != null ? (
+          <img src={profileImage} alt={name + "Avatar"} />
+        ) : (
+          <Icon
+            path={fallbackIcon}
+            size={iconWidth + "px"}
+            style={{
+              left: iconWidth / 2,
+              top: iconWidth / 2,
+              position: "absolute",
+            }}
+          />
+        )}
+      </AspectRatio>
+      <Link href={"#"} overlay sx={OVERLAY_SX}>
+        {name}
+      </Link>
+    </Card>
   );
 };
