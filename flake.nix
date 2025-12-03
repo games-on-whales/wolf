0c2253705263fd69495d4458352912298fc540c2
Mehdi Benadel <mehdi.benadel@gmail.com>
2025-12-03 02:42:54 +0100

    flake setup + fix shebang

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Dev Environment flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = import ./shell.nix { inherit pkgs; };
+  };
+}
